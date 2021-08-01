ed4b970201d80752e7ffbd7bf4fe3888561bfd0e
Ivan <ivan.kavtarev@mail.ru>
2021-08-01 20:36:24 +0300

    first of many

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Button, Container, TextField } from '@material-ui/core'
+import SendRoundedIcon from '@material-ui/icons/SendRounded'
+import './App.css'
+
+import { useStyle } from './style/matHook'
+import { useState } from 'react'
 
 function App() {
+  let [name, setName] = useState('')
+  let [message, setMessage] = useState('')
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(name, message)
+  }
+
+  let style = useStyle()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Container>
+      <Typography
+        variant='body1'
+        component='h2'
+        color='textPrimary'
+        align='center'
+      >
+        hi there
+      </Typography>
+      <form onSubmit={submitHandler}>
+        <TextField
+          required
+          onChange={(e) => setName(e.target.value)}
+          variant='outlined'
+          fullWidth
+          size='small'
+          margin='dense'
+          label='name'
+          // inputProps={{ style: { fontSize: 40 } }}
+        />
+        <TextField
+          required
+          onChange={(e) => setMessage(e.target.value)}
+          variant='outlined'
+          fullWidth
+          multiline
+          minRows='4'
+          size='medium'
+          margin='normal'
+          label='message text'
+        />
+        <Button
+          variant='contained'
+          color='primary'
+          endIcon={<SendRoundedIcon color='secondary' />}
+          type='submit'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Primary
+        </Button>
+      </form>
+    </Container>
+  )
 }
 
-export default App;
+export default App
